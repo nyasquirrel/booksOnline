e61741f0f8b45a15217c3621fbeb0f26ff855e06
squirrel <seo.squirrel@yandex.ru>
2019-12-06 23:00:09 +0300

    сверстал страницу, практикую js на сортировке

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,38 @@ $(document).ready(function () {
         loop: true,
         dots: false,
         nav: true,
-        navText: ['<img src="./img/nav-prev.png" alt="Левая стрелка">','<img src="./img/nav-next.png" alt="Правая стрелка">']
+        navText: ['<img src="./img/nav-prev.png" alt="Левая стрелка">', '<img src="./img/nav-next.png" alt="Правая стрелка">']
     });
 });
 
 
-let productItems = document.querySelectorAll('.production__item')
+let tabs = document.querySelectorAll('.production__tabs-link');
+let productItems = document.querySelectorAll('.production__item');
+let productsBody = document.querySelector('.production__listing');
+
+for (tab of tabs) {
+    tab.addEventListener('click', e => {
+        e.preventDefault();
+        let sortName = tab.getAttribute('href');
+        switch (sortName) {
+            case '#special':
+                showSpecialProducts();
+                break;
+            default:
+                showAllProducts();
+        }
+    })
+}
+
+function fromArray() {
+    let arrlist = Array.from(productItems);
+}
+function showSpecialProducts() {
+    let productItemsArray = Array.from(productItems);
+    let specialItems = productItemsArray.filter(el =>
+        el.dataset.item == 'special'
+    );
+    productsBody.innerHTML = '';
+    for (item of specialItems)
+        productsBody.innerHTML += item;
+}
